7867690b30e0c69f10ea301c99b56daf014c002b
Jakub <kubuskrol@gmail.com>
2026-03-13 19:55:47 +0100

    chore: add Nuxt ESlint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from 'nuxt/config'
-
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxt/ui',
-    '@nuxtjs/i18n'
-  ],
-  css: ['~/assets/css/main.css'],
-  i18n: {
-    defaultLocale: 'en',
-    locales: [
-      { code: 'en', name: 'English', file: 'en.json' },
-      { code: 'pl', name: 'Polski', file: 'pl.json' }
-    ]
-  }
-})
+	modules: ["@nuxt/ui", "@nuxtjs/i18n", "@nuxt/eslint"],
+	devtools: { enabled: true },
+	css: ["~/assets/css/main.css"],
+	compatibilityDate: "2025-07-15",
+	eslint: {
+		config: {
+			stylistic: {
+				semi: true,
+				quotes: "double",
+				commaDangle: "always-multiline",
+				indent: "tab",
+			},
+		},
+	},
+	i18n: {
+		defaultLocale: "en",
+		locales: [
+			{ code: "en", name: "English", file: "en.json" },
+			{ code: "pl", name: "Polski", file: "pl.json" },
+		],
+	},
+});
