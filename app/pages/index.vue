601e5d1dddff350cce87e05ff0a66e4623fae879
Jakub <kubuskrol@gmail.com>
2026-03-13 18:10:17 +0100

    feat: add Nuxt i18n

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,14 @@
+<script setup>
+const { locales, setLocale } = useI18n()
+</script>
+
 <template>
-  <h1>Index page</h1>
+  <div>
+    <div>
+        <UButton v-for="locale in locales" @click="setLocale(locale.code)">
+            {{ locale.name }}
+        </UButton>
+    </div>
+    <h1>{{ $t('hello-world') }}</h1>
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,16 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
-  css: ['~/assets/css/main.css']
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/i18n'
+  ],
+  css: ['~/assets/css/main.css'],
+  i18n: {
+    defaultLocale: 'en',
+    locales: [
+      { code: 'en', name: 'English', file: 'en.json' },
+      { code: 'pl', name: 'Polski', file: 'pl.json' }
+    ]
+  }
 })
