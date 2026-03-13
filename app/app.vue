53aee10b6d42710f811b873d6fdf6f03cb61d5b2
Jakub <kubuskrol@gmail.com>
2026-03-13 21:19:25 +0100

    feat: head and SEO Meta

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,38 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+const { locale } = useI18n();
+
+useHead({
+	htmlAttrs: {
+		lang: locale,
+	},
+	titleTemplate: (title) => {
+		return title
+			? `${title} | ${config.public.appName}`
+			: config.public.appName;
+	},
+	link: [
+		{ rel: "icon", type: "image/png", href: "/favicon-96x96.png", sizes: "96x96" },
+		{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+		{ rel: "shortcut icon", href: "/favicon.ico" },
+		{ rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+		{ rel: "manifest", href: "/site.webmanifest" },
+		{ rel: "canonical", href: config.public.appURL },
+		{ rel: "alternate", hreflang: "pl", href: `${config.public.appURL}/pl` },
+		{ rel: "alternate", hreflang: "en", href: `${config.public.appURL}/en` },
+		{ rel: "alternate", hreflang: "x-default", href: config.public.appURL },
+	],
+});
+
+useSeoMeta({
+	ogSiteName: config.public.appName,
+	ogType: "website",
+	ogUrl: config.public.appURL,
+
+	themeColor: "#3b82f6",
+});
+</script>
+
 <template>
 	<UApp>
 		<NuxtAnnouncer />
