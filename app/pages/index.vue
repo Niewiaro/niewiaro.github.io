f7df1c6cc77f60ddf605d408c6e7ff7a1ce1a8a8
Jakub <kubuskrol@gmail.com>
2026-03-13 19:56:10 +0100

    chore: ESlint

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,17 @@
 <script setup>
-const { locales, setLocale } = useI18n()
+const { locales, setLocale } = useI18n();
 </script>
 
 <template>
-  <div>
-    <div>
-        <UButton v-for="locale in locales" @click="setLocale(locale.code)">
-            {{ locale.name }}
-        </UButton>
-    </div>
-    <h1>{{ $t('hello-world') }}</h1>
-  </div>
+	<div>
+		<div>
+			<UButton
+				v-for="locale in locales"
+				@click="setLocale(locale.code)"
+			>
+				{{ locale.name }}
+			</UButton>
+		</div>
+		<h1>{{ $t('hello-world') }}</h1>
+	</div>
 </template>
