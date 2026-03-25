684b9c751b405fc02601faa0a9e17fa395e89a56
Jakub <kubuskrol@gmail.com>
2026-03-26 00:25:34 +0100

    feat: add Hello World to links

--- app/components/app/Header.vue
+++ app/components/app/Header.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
 const config = useRuntimeConfig();
 const localePath = useLocalePath();
+
+const items = computed<NavigationMenuItem[]>(() => [{
+	label: "Hello World",
+	icon: "i-mdi-cube-outline",
+	to: "https://helloworld.niewiaro.cc/",
+	target: "_blank",
+}]);
 </script>
 
 <template>
@@ -19,6 +28,8 @@ const localePath = useLocalePath();
 				<span class="font-bold text-lg">{{ config.public.appName }}</span>
 			</NuxtLink>
 		</template>
+
+		<UNavigationMenu :items="items" />
 
 		<template #right>
 			<UColorModeButton />
