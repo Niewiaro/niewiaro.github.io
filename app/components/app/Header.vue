537e87aabcdadb698b77ded24ca3d119b979cdb1
Jakub <kubuskrol@gmail.com>
2026-03-26 01:07:16 +0100

    feat: header Select language

--- app/components/app/Header.vue
+++ app/components/app/Header.vue
@@ -1,15 +1,26 @@
 <script setup lang="ts">
-import type { NavigationMenuItem } from "@nuxt/ui";
+import type { NavigationMenuItem, DropdownMenuItem } from "@nuxt/ui";
 
 const config = useRuntimeConfig();
 const localePath = useLocalePath();
 
+const { t, locale, locales } = useI18n();
+const switchLocalePath = useSwitchLocalePath();
+
 const items = computed<NavigationMenuItem[]>(() => [{
-	label: "Hello World",
+	label: t("hello-world"),
 	icon: "i-mdi-cube-outline",
 	to: "https://helloworld.niewiaro.cc/",
 	target: "_blank",
 }]);
+
+const languageItems = computed<DropdownMenuItem[]>(() =>
+	locales.value.map(l => ({
+		label: l.name,
+		to: switchLocalePath(l.code),
+		slot: l.code,
+	})),
+);
 </script>
 
 <template>
@@ -32,6 +43,33 @@ const items = computed<NavigationMenuItem[]>(() => [{
 		<UNavigationMenu :items="items" />
 
 		<template #right>
+			<UDropdownMenu
+				:items="languageItems"
+				:content="{
+					align: 'end',
+					side: 'bottom',
+				}"
+			>
+				<UButton
+					icon="i-lucide-languages"
+					color="neutral"
+					variant="ghost"
+					:aria-label="t('i18n.selectLanguage')"
+				/>
+
+				<template
+					v-for="l in locales"
+					:key="l.code"
+					#[`${l.code}-trailing`]
+				>
+					<UIcon
+						v-if="l.code === locale"
+						name="i-lucide-check"
+						class="shrink-0 size-5 text-primary"
+					/>
+				</template>
+			</UDropdownMenu>
+
 			<UColorModeButton />
 		</template>
 	</UHeader>
